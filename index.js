f7759ce87a8b0fa5bc1bb02a6d52db1aedd13374
Kauê da Maia <kaue@ruptiva.com>
2019-02-15 15:02:43 -0200

    fix reset method

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const generateActions = (initialState, prefix) => {
 
 export const generateReducer = (initialState, prefix) => {
   return (state = initialState, { type, payload }) => {
-    const actions = [...generateActions(initialState, prefix), generateType('reset', prefix)]
+    const actions = [...generateActions(initialState, prefix), `${prefix}/RESET`]
 
     const currentType = actions.indexOf(type) > -1 ? actions[actions.indexOf(type)] : null
 
-    const field = currentType ? generateField(currentType.toLowerCase()) : ''
-
     if (currentType) {
-      return currentType !== `${prefix}/RESET`
-        ? { ...state, [field]: payload || initialState[field] }
-        : { ...initialState }
+      const isReset = currentType === `${prefix}/RESET`
+      const field = !isReset ? generateField(currentType.toLowerCase()) : ''
+
+      return !isReset ? { ...state, [field]: payload || initialState[field] } : { ...initialState }
     }
 
     return state
   }
 }
 
-export const reset = prefix => ({ type: generateType('reset', prefix) })
+export const reset = prefix => ({ type: `${prefix}/RESET` })
 export const setField = (prefix, field, payload) => ({ type: generateType(field, prefix), payload })
